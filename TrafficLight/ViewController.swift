81dbb458365e858d6cafe3dbffce5f8189f1ba4c
NetRunner <galatskiy@gmail.com>
2023-04-21 17:09:45 +0300

    Final build with active shadows and correct sizes on all models

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,50 +7,60 @@
 
 import UIKit
 
-enum SelectedColor {
-    case red
-    case yellow
-    case green
-}
-
-var currentSelectedColor = SelectedColor.red
-
 final class ViewController: UIViewController {
-
+    
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
 
     @IBOutlet var startButton: UIButton!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        redLight.layer.cornerRadius = 69
-        yellowLight.layer.cornerRadius = 69
-        greenLight.layer.cornerRadius = 69
-        startButton.layer.cornerRadius = 10
+    private enum SelectedColor {
+        case red
+        case yellow
+        case green
     }
 
+    private var currentSelectedColor = SelectedColor.red
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startButton.layer.cornerRadius = 10
+    }
+    
+    override func viewDidLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+    }
+    
     @IBAction func startButtonTapped() {
         if startButton.isEnabled {
             startButton.setTitle("NEXT", for: .normal)
         }
         
-        if startButton.isTouchInside {
-            switch currentSelectedColor {
-            case .red:
-                greenLight.alpha = 0.3
-                redLight.alpha = 1
-                currentSelectedColor = .yellow
-            case .yellow:
-                redLight.alpha = 0.3
-                yellowLight.alpha = 1
-                currentSelectedColor = .green
-            case .green:
-                yellowLight.alpha = 0.3
-                greenLight.alpha = 1
-                currentSelectedColor = .red
-            }
+        switch currentSelectedColor {
+        case .red:
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+            redLight.layer.shadowColor = UIColor.red.cgColor
+            redLight.layer.shadowRadius = 50
+            redLight.layer.shadowOpacity = 1
+            currentSelectedColor = .yellow
+        case .yellow:
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+            yellowLight.layer.shadowColor = UIColor.yellow.cgColor
+            yellowLight.layer.shadowRadius = 50
+            yellowLight.layer.shadowOpacity = 1
+            currentSelectedColor = .green
+        case .green:
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+            greenLight.layer.shadowColor = UIColor.green.cgColor
+            greenLight.layer.shadowRadius = 50
+            greenLight.layer.shadowOpacity = 1
+            currentSelectedColor = .red
         }
     }
 }
