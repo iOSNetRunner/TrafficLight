8ba24a1b9bb10d1d6f6130c83660d35de9a79637
NetRunner <galatskiy@gmail.com>
2023-04-21 15:27:19 +0300

    Button switch colors logic ready

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,9 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum SelectedColor {
+    case red
+    case yellow
+    case green
+}
 
-    
+var currentSelectedColor = SelectedColor.red
+
+final class ViewController: UIViewController {
+
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
@@ -21,13 +28,29 @@ class ViewController: UIViewController {
         redLight.layer.cornerRadius = 69
         yellowLight.layer.cornerRadius = 69
         greenLight.layer.cornerRadius = 69
-        startButton.layer.cornerRadius = 20
+        startButton.layer.cornerRadius = 10
     }
 
     @IBAction func startButtonTapped() {
         if startButton.isEnabled {
             startButton.setTitle("NEXT", for: .normal)
         }
+        
+        if startButton.isTouchInside {
+            switch currentSelectedColor {
+            case .red:
+                greenLight.alpha = 0.3
+                redLight.alpha = 1
+                currentSelectedColor = .yellow
+            case .yellow:
+                redLight.alpha = 0.3
+                yellowLight.alpha = 1
+                currentSelectedColor = .green
+            case .green:
+                yellowLight.alpha = 0.3
+                greenLight.alpha = 1
+                currentSelectedColor = .red
+            }
+        }
     }
 }
-
