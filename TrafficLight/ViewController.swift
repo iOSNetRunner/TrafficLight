0d0176295d8eb98be336055f1d54c7f55bc8a47b
NetRunner <galatskiy@gmail.com>
2023-04-21 03:42:44 +0300

    startButton & cornerRadius ready

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.layer.cornerRadius = 69
+        yellowLight.layer.cornerRadius = 69
+        greenLight.layer.cornerRadius = 69
+        startButton.layer.cornerRadius = 20
     }
 
-
+    @IBAction func startButtonTapped() {
+        if startButton.isEnabled {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+    }
 }
 
